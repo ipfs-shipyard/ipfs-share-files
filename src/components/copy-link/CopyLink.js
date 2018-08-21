be2eb3cf6f93893452a5c3cec78f805407faed06
Diogo Silva <fsdiogo@gmail.com>
2018-08-21 17:49:42 +0100

    chore: update CopyLink UI (#13)

---
+++ src/components/copy-link/CopyLink.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import classnames from 'classnames'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
+
+class CopyLink extends React.Component {
+  static propTypes = {
+    shareLink: PropTypes.string.isRequired,
+    withLabel: PropTypes.bool.isRequired
+  }
+
+  static defaultProps = {
+    shareLink: '',
+    withLabel: true
+  }
+
+  state = {
+    copied: false
+  }
+
+  render () {
+    const { shareLink } = this.props
+    const copyBtnClass = classnames({
+      'o-50': this.state.copied,
+      'o-80 glow pointer': !this.state.copied
+    }, ['pa2 w-20 flex items-center justify-center br-pill bg-navy f7 white'])
+
+    return (
+      <div className='mb4'>
+        { this.props.withLabel && <div className='f7 gray'>Copy link to send files:</div> }
+        <div className='pa1 mt3 w-100 flex items-center br-pill bg-light-gray'>
+          <div className='ph2 w-80 f7 navy truncate'>
+            { shareLink }
+          </div>
+          <CopyToClipboard text={shareLink} onCopy={this.handleOnCopyClick}>
+            <div className={copyBtnClass} style={{ pointerEvents: this.state.copied && 'none' }}>
+              { this.state.copied ? 'Copied!' : 'Copy' }
+            </div>
+          </CopyToClipboard>
+        </div>
+      </div>
+    )
+  }
+
+  handleOnCopyClick = () => {
+    this.setState(
+      {copied: true},
+      () => setTimeout(() => this.setState({copied: false}), 2500)
+    )
+  }
+}
+
+export default CopyLink
