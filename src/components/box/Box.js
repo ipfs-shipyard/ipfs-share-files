24c3bb9a3d34aa73a96979f239e9378a3b0c71b6
Diogo Silva <fsdiogo@gmail.com>
2018-08-21 17:49:01 +0100

    chore: update CopyLink UI

--- src/components/box/Box.js
+++ src/components/box/Box.js
@@ -1,26 +1,10 @@
 import React from 'react'
 import { connect } from 'redux-bundler-react'
-import { CopyToClipboard } from 'react-copy-to-clipboard'
 
 // Components
 import FileAdd from '../file//file-add/FileAdd'
 import FileTree from '../file//file-tree/FileTree'
-
-const CopyLink = ({ shareLink }) => (
-  <div className='mb4'>
-    <div className='f7 gray'>Copy link to send files:</div>
-    <div className='pa1 mt3 w-100 flex items-center br-pill bg-light-gray'>
-      <div className='ph2 w-80 f7 navy truncate'>
-        { shareLink }
-      </div>
-      <CopyToClipboard text={shareLink}>
-        <div className='pa2 w-20 flex items-center justify-center br-pill bg-navy f7 white o-80 glow pointer'>
-          Copy
-        </div>
-      </CopyToClipboard>
-    </div>
-  </div>
-)
+import CopyLink from '../copy-link//CopyLink'
 
 const Footnote = () => (
   <div className='f7 gray lh-copy'>
