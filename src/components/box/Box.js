535a3d0ebe3c51df88293220035d0a3773283c43
Henrique Dias <hacdias@gmail.com>
2018-09-13 11:29:21 +0100

    wip: simplify box
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- src/components/box/Box.js
+++ src/components/box/Box.js
@@ -7,35 +7,22 @@ import FileTree from '../file-tree/FileTree'
 import CopyLink from '../copy-link//CopyLink'
 import DownloadFiles from '../download-files//DownloadFiles'
 
-const Footnote = () => (
-  <div className='f7 gray lh-copy'>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-  </div>
-)
-
 const Box = ({ isDownload, files, shareLink, isLoading }) => {
   const boxClass = 'mb4 mb0-l pa4 w-100 w-third-l mw6 order-2-l br3 shadow-4 bg-white'
 
-  const renderUpload = () => (
+  return (
     <div className={boxClass}>
-      <AddFiles />
+      { !isDownload && <AddFiles /> }
       { isLoading && <Loader /> }
       <FileTree files={files} isDownload={isDownload} />
-      { shareLink && <CopyLink shareLink={shareLink} /> }
-      <Footnote />
+      { !isDownload && shareLink && <CopyLink shareLink={shareLink} /> }
+      { isDownload && <DownloadFiles /> }
+
+      <div className='f7 gray lh-copy'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </div>
     </div>
   )
-
-  const renderDownload = () => (
-    <div className={boxClass}>
-      { isLoading && <Loader /> }
-      <FileTree files={files} isDownload />
-      <DownloadFiles />
-      <Footnote />
-    </div>
-  )
-
-  return isDownload ? renderDownload() : renderUpload()
 }
 
 export default Box
