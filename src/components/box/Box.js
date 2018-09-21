a47634238a25b95d1c2a67a66293b691e5f7f6bc
Diogo Silva <fsdiogo@gmail.com>
2018-09-21 18:31:03 +0100

    chore: update missing daemon layout

--- src/components/box/Box.js
+++ src/components/box/Box.js
@@ -8,16 +8,11 @@ import FileTree from '../file-tree/FileTree'
 import CopyLink from '../copy-link/CopyLink'
 import DownloadFiles from '../download-files/DownloadFiles'
 
-export const RawBox = ({ children, t }) => (
+export const Box = ({ children }) => (
   <div className='mb4 mb0-l pa4 w-100 w-third-l mw6 order-2-l br3 shadow-4 bg-white'>
     { children }
-    <div className='f7 gray lh-copy'>
-      {t('box.footNote')}
-    </div>
   </div>
 )
-
-export const Box = translate()(RawBox)
 
 export const BoxDownload = ({ files, isLoading }) => (
   <Box>
@@ -27,20 +22,29 @@ export const BoxDownload = ({ files, isLoading }) => (
   </Box>
 )
 
-export const BoxUpload = ({ files, isLoading, shareLink }) => (
+export const RawBoxUpload = ({ files, isLoading, shareLink, t }) => (
   <Box>
     <AddFiles />
     { isLoading && <Loader /> }
     <FileTree files={files} />
     { shareLink && <CopyLink shareLink={shareLink} /> }
+    <div className='f7 gray lh-copy'>
+      {t('box.footNote')}
+    </div>
   </Box>
 )
 
+export const BoxUpload = translate()(RawBoxUpload)
+
 export const RawBoxNotAvailable = ({ t }) => (
   <Box>
-    <p className='mt0 navy lh-copy'>
-      {t('box.missingDaemon')}
-    </p>
+    <p className='mv0 orange f5 lh-title'>{t('box.missingDaemon')}</p>
+    <p className='mv3 navy f6 lh-copy'>{t('box.runDaemon')}</p>
+    <div className='db pa3 bg-black-80 bt bw4 br2 gray-muted f7'>
+      <code className='db'>$ ipfs daemon</code>
+      <code className='db'>Initializing daemon...</code>
+      <code className='db truncate'>API server listening on /ip4/127.0.0.1/tcp/5001</code>
+    </div>
   </Box>
 )
 
