c7563ccddf6393c83f0dc03b6178ef0816cb4476
Diogo Silva <fsdiogo@gmail.com>
2018-11-27 18:37:41 +0000

    chore: add page copy

--- src/components/info/Info.js
+++ src/components/info/Info.js
@@ -2,35 +2,58 @@ import React from 'react'
 import { translate } from 'react-i18next'
 
 // Static
-import addImg from '../../media/images/add.svg'
+import DownloadIcon from '../../media/icons/StrokeDownload'
+import ShareIcon from '../../media/icons/StrokeShare'
+import CubeIcon from '../../media/icons/StrokeCube'
 
-const Info = ({ t, imgHeigth = 110 }) => {
-  const labelClass = 'mv3 white montserrat'
-  const copyClass = 'f6 gray lh-copy'
-  const copyStyle = { flexBasis: '100%' }
+const Info = ({ t, imgHeight = 70 }) => {
+  const iconContainerClass = 'mr3 fill-aqua'
+  const labelClass = 'mb3 white montserrat'
+  const numberClass = 'mr2 aqua'
+  const descriptionClass = 'f6 gray lh-copy'
 
   return (
     <div className='pa4 pr5-l w-100 w-two-thirds-l mw7-l self-start order-1-l'>
-      <div className='mb3 f2 white montserrat'>{t('info.title')}</div>
-      <div className='mb5 f5 gray lh-copy'>{t('info.description')}</div>
+      <div className='mb5 f2 white montserrat'>{t('info.title')}</div>
 
-      <div className='flex'>
-        <div className='pa3 tc' style={copyStyle}>
-          <img src={addImg} height={imgHeigth} alt='Add' />
-          <div className={labelClass}>{t('info.labelAdding')}</div>
-          <div className={copyClass}>{t('info.copyAdding')}</div>
+      <div className='flex flex-column'>
+        <div className='pa3 flex'>
+          <div className={iconContainerClass}>
+            <DownloadIcon height={imgHeight} style={{ marginTop: '-11px' }} alt='Download' />
+          </div>
+          <div clasName='flex'>
+            <div className={labelClass}>
+              <span className={numberClass}>1.</span>
+              {t('info.labelAdd')}
+            </div>
+            <div className={descriptionClass}>{t('info.copyAdd')}</div>
+          </div>
         </div>
 
-        <div className='pa3 tc' style={copyStyle}>
-          <img src={addImg} height={imgHeigth} alt='Add' />
-          <div className={labelClass}>{t('info.labelNodes')}</div>
-          <div className={copyClass}>{t('info.copyNodes')}</div>
+        <div className='pa3 mv2 flex'>
+          <div className={iconContainerClass}>
+            <ShareIcon height={imgHeight} style={{ marginTop: '-13px' }} alt='Share' />
+          </div>
+          <div clasName='flex'>
+            <div className={labelClass}>
+              <span className={numberClass}>2.</span>
+              {t('info.labelSend')}
+            </div>
+            <div className={descriptionClass}>{t('info.copySend')}</div>
+          </div>
         </div>
 
-        <div className='pa3 tc' style={copyStyle}>
-          <img src={addImg} height={imgHeigth} alt='Add' />
-          <div className={labelClass}>{t('info.labelGetting')}</div>
-          <div className={copyClass}>{t('info.copyGetting')}</div>
+        <div className='pa3 flex'>
+          <div className={iconContainerClass}>
+            <CubeIcon height={imgHeight} style={{ marginTop: '-3px' }} alt='Cube' />
+          </div>
+          <div clasName='flex'>
+            <div className={labelClass}>
+              <span className={numberClass}>3.</span>
+              {t('info.labelKeep')}
+            </div>
+            <div className={descriptionClass}>{t('info.copyKeep')}</div>
+          </div>
         </div>
       </div>
     </div>
