fddcfa074b49b650c57b6d9639fc70400f8b8a78
Diogo Silva <fsdiogo@gmail.com>
2018-11-28 11:58:53 +0000

    chore: add & download copy

--- src/components/info/Info.js
+++ src/components/info/Info.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { connect } from 'redux-bundler-react'
 import { translate } from 'react-i18next'
 
 // Static
@@ -6,27 +7,80 @@ import DownloadIcon from '../../media/icons/StrokeDownload'
 import ShareIcon from '../../media/icons/StrokeShare'
 import CubeIcon from '../../media/icons/StrokeCube'
 
-const Info = ({ t, imgHeight = 70 }) => {
+const Info = ({ t, imgHeight = 70, isDownload, ipfsProvider }) => {
   const iconContainerClass = 'mr3 fill-aqua'
   const labelClass = 'mb3 white montserrat'
   const numberClass = 'mr2 aqua'
   const descriptionClass = 'f6 gray lh-copy'
 
+  const isUsingDaemon = ipfsProvider === 'window.ipfs' || ipfsProvider === 'js-ipfs-api'
+
+  // Info for the Download page
+  if (isDownload) {
+    return (
+      <div className='pa4 pr5-l w-100 w-two-thirds-l mw7-l self-start order-1-l'>
+        <div className='mb5 f2 white montserrat'>{t('info.download.title')}</div>
+
+        <div className='flex flex-column'>
+          <div className='pa3 flex'>
+            <div className={iconContainerClass}>
+              <DownloadIcon height={imgHeight} style={{ marginTop: '-11px' }} alt='Download' />
+            </div>
+            <div>
+              <div className={labelClass}>
+                <span className={numberClass}>1.</span>
+                {t('info.download.labelGet')}
+              </div>
+              <div className={descriptionClass}>{t('info.download.copyGet')}</div>
+            </div>
+          </div>
+
+          <div className='pa3 flex'>
+            <div className={iconContainerClass}>
+              <CubeIcon height={imgHeight} style={{ marginTop: '-3px' }} alt='Cube' />
+            </div>
+            <div>
+              <div className={labelClass}>
+                <span className={numberClass}>2.</span>
+                { isUsingDaemon ? t('info.download.labelKeepDaemon') : t('info.download.labelKeepPage') }
+              </div>
+              <div className={descriptionClass}>{t('info.download.copyKeep')}</div>
+            </div>
+          </div>
+
+          <div className='pa3 mv2 flex'>
+            <div className={iconContainerClass}>
+              <ShareIcon height={imgHeight} style={{ marginTop: '-13px' }} alt='Share' />
+            </div>
+            <div>
+              <div className={labelClass}>
+                <span className={numberClass}>3.</span>
+                {t('info.download.labelSend')}
+              </div>
+              <div className={descriptionClass}>{t('info.download.copySend')}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  // Info for the Add page
   return (
     <div className='pa4 pr5-l w-100 w-two-thirds-l mw7-l self-start order-1-l'>
-      <div className='mb5 f2 white montserrat'>{t('info.title')}</div>
+      <div className='mb5 f2 white montserrat'>{t('info.add.title')}</div>
 
       <div className='flex flex-column'>
         <div className='pa3 flex'>
           <div className={iconContainerClass}>
             <DownloadIcon height={imgHeight} style={{ marginTop: '-11px' }} alt='Download' />
           </div>
-          <div clasName='flex'>
+          <div>
             <div className={labelClass}>
               <span className={numberClass}>1.</span>
-              {t('info.labelAdd')}
+              {t('info.add.labelAdd')}
             </div>
-            <div className={descriptionClass}>{t('info.copyAdd')}</div>
+            <div className={descriptionClass}>{t('info.add.copyAdd')}</div>
           </div>
         </div>
 
@@ -34,12 +88,12 @@ const Info = ({ t, imgHeight = 70 }) => {
           <div className={iconContainerClass}>
             <ShareIcon height={imgHeight} style={{ marginTop: '-13px' }} alt='Share' />
           </div>
-          <div clasName='flex'>
+          <div>
             <div className={labelClass}>
               <span className={numberClass}>2.</span>
-              {t('info.labelSend')}
+              {t('info.add.labelSend')}
             </div>
-            <div className={descriptionClass}>{t('info.copySend')}</div>
+            <div className={descriptionClass}>{t('info.add.copySend')}</div>
           </div>
         </div>
 
@@ -47,12 +101,12 @@ const Info = ({ t, imgHeight = 70 }) => {
           <div className={iconContainerClass}>
             <CubeIcon height={imgHeight} style={{ marginTop: '-3px' }} alt='Cube' />
           </div>
-          <div clasName='flex'>
+          <div>
             <div className={labelClass}>
               <span className={numberClass}>3.</span>
-              {t('info.labelKeep')}
+              {isUsingDaemon ? t('info.add.labelKeepDaemon') : t('info.add.labelKeepPage')}
             </div>
-            <div className={descriptionClass}>{t('info.copyKeep')}</div>
+            <div className={descriptionClass}>{t('info.add.copyKeep')}</div>
           </div>
         </div>
       </div>
@@ -60,4 +114,9 @@ const Info = ({ t, imgHeight = 70 }) => {
   )
 }
 
-export default translate()(Info)
+export const TranslatedInfo = translate()(Info)
+
+export default connect(
+  'selectIpfsProvider',
+  TranslatedInfo
+)
--- src/components/info/Info.stories.js
+++ src/components/info/Info.stories.js
@@ -2,13 +2,18 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { checkA11y } from '@storybook/addon-a11y'
 import i18n from '../../i18n-decorator'
-import Info from './Info'
+import { TranslatedInfo as Info } from './Info'
 
 storiesOf('Info', module)
   .addDecorator(checkA11y)
   .addDecorator(i18n)
-  .add('Default', () => (
+  .add('Add', () => (
     <div className='bg-navy sans-serif'>
       <Info />
+    </div>
+  ))
+  .add('Download', () => (
+    <div className='bg-navy sans-serif'>
+      <Info isDownload />
     </div>
   ))
