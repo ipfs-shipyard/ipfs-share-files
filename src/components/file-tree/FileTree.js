78182e479211a2e7d4a60bc80994b9d4819b8359
Jessica Schilling <jessica@protocol.ai>
2021-02-01 17:36:19 -0700

    Better subtitles, tidy up type spacing

--- src/components/file-tree/FileTree.js
+++ src/components/file-tree/FileTree.js
@@ -1,25 +1,31 @@
 import React from 'react'
+import { withTranslation, Trans } from 'react-i18next'
 
 // Components
 import File from '../file/File'
 
 const FileTree = ({ files, isDownload }) => (
-  <div className='mb4'>
+  <div>
     { files
       ? Object.entries(files).map(([id, file]) =>
-      <File
-        key={`file-${id}`}
-        id={file.id}
-        cid={file.cid}
-        name={file.name}
-        size={file.size}
-        type={file.type}
-        progress={file.progress}
-        error={file.error}
-        isDownload={isDownload} />
+      <div>
+        <div className='f6 fw6 charcoal mt4 mb2'>
+          <Trans i18nKey='copyLink.labelIndividual'>Share individual files:</Trans>
+        </div>
+        <File
+          key={`file-${id}`}
+          id={file.id}
+          cid={file.cid}
+          name={file.name}
+          size={file.size}
+          type={file.type}
+          progress={file.progress}
+          error={file.error}
+          isDownload={isDownload} />
+        </div>
       )
       : null }
   </div>
 )
 
-export default FileTree
+export default withTranslation('translation')(FileTree)
