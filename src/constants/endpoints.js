8c2c53147cf8fff6bceae8e858357d01ff4439ea
Marcin Rataj <lidel@lidel.org>
2021-02-02 23:25:30 +0100

    feat: use dweb.link instead of ipfs.io
    Closes #94

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,5 +1,5 @@
 const endpoints = {
-  gateway: 'https://ipfs.io/ipfs',
+  gateway: 'https://dweb.link/ipfs',
   api: 'https://ipfs.io/api',
   share: 'https://share.ipfs.io/#',
   apiMultiAddr: '/dns4/ipfs.io/tcp/443/https'
