faf9b50ad2f4fa8388738f208d2d922eb6990f7d
Rafael Ramalho <rafazelramalho19@gmail.com>
2021-02-04 23:12:40 +0000

    feat: add drag-n-drop to the add files box (#100)
    * feat: add drag-n-drop to tbe add files box
    * fix: disable dnd for directories
    
    Co-authored-by: terichadbourne <terichad@gmail.com>
    Co-authored-by: Marcin Rataj <lidel@lidel.org>

--- src/index.js
+++ src/index.js
@@ -6,12 +6,16 @@ import registerServiceWorker from './registerServiceWorker'
 import getStore from './bundles'
 import { I18nextProvider } from 'react-i18next'
 import i18n from './i18n'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import { DndProvider } from 'react-dnd'
 
 ReactDOM.render(
   <Provider store={getStore()}>
-    <I18nextProvider i18n={i18n} >
-      <App />
-    </I18nextProvider>
+    <DndProvider backend={HTML5Backend}>
+      <I18nextProvider i18n={i18n} >
+        <App />
+      </I18nextProvider>
+    </DndProvider>
   </Provider>, document.getElementById('root'))
 
 registerServiceWorker()
