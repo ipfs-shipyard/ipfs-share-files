3bdfe85db009028fc299e30990ef050e4a4c552c
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-10-08 17:43:05 -0500

    fix: blur QR code until root is published

--- src/components/share-all-files/share-all-files.tsx
+++ src/components/share-all-files/share-all-files.tsx
@@ -29,12 +29,12 @@ export const ShareAllFiles = ({ withLabel }: { withLabel?: boolean }): React.Rea
   const shouldGenerateLink = useMemo(() => Object.keys(files).length > 0, [files])
   const allFilesArePublished = useMemo(() => Object.values(files).every((file) => file.published), [files])
 
-  useEffect(() => {})
+  const disabled = !allFilesArePublished || !rootPublished
 
   const copyBtnClass = classnames({
     'o-50 no-pointer-events': copied,
     'o-80 glow pointer': !copied,
-    'o-50 no-pointer-events bg-gray': !allFilesArePublished || !rootPublished
+    'o-50 no-pointer-events bg-gray': disabled
   }, ['pa2 w3 flex items-center justify-center br-pill bg-navy f7 white'])
 
   if (mfs == null || helia == null) {
@@ -68,20 +68,22 @@ export const ShareAllFiles = ({ withLabel }: { withLabel?: boolean }): React.Rea
       <div className="overflow-hidden">
         <div className="flex flex-column items-center mb3 appear-from-below">
           <span className="f7 charcoal-muted lh-copy pb2">{t('copyLink.qrLabel')}</span>
-          <QRCodeSVG
-            value={shareAllLink}
-            bgColor={'#ffffff'}
-            fgColor={'#022E44'}
-            level={'M'}
-            imageSettings={{
-              src: 'favicon-32x32.png',
-              x: 50,
-              y: 50,
-              height: 32,
-              width: 32,
-              excavate: true
-            }}
-          />
+          <span style={ disabled ? { filter: 'blur(2px)' } : {} }>
+            <QRCodeSVG
+              value={shareAllLink}
+              bgColor={'#ffffff'}
+              fgColor={'#022E44'}
+              level={'M'}
+              imageSettings={{
+                src: 'favicon-32x32.png',
+                x: 50,
+                y: 50,
+                height: 32,
+                width: 32,
+                excavate: true
+              }}
+            />
+          </span>
         </div>
       </div>
     </div>
