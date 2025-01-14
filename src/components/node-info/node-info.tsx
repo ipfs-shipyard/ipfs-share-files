3dd4e1e1b19200034b99c7274ea1fd17f0868a11
Daniel N <2color@users.noreply.github.com>
2025-01-14 12:14:00 +0100

    fix: refine dialability in node info box

--- src/components/node-info/node-info.tsx
+++ src/components/node-info/node-info.tsx
@@ -16,13 +16,13 @@ export const NodeInfo: React.FC<NodeInfoProps> = () => {
 
   const { listeningAddrs, circuitRelayAddrs, webRtc, webRtcDirect, webTransport, webSockets, webSocketsSecure } = useMemo(() => {
     const base = {
-      listeningAddrs: 0,
-      circuitRelayAddrs: 0,
-      webRtc: 0,
-      webRtcDirect: 0,
-      webTransport: 0,
-      webSockets: 0,
-      webSocketsSecure: 0
+      listeningAddrs: 0, // total listening addrs
+      circuitRelayAddrs: 0, // circuit relay addrs
+      webRtc: 0, // Any WebRTC address including a TCP relay one which isn't very useful for other browsers
+      webRtcDirect: 0, // WebRTC Direct circuit relay addrs which can be used for browser WebRTC signalling
+      webTransport: 0, // WebTransport circuit relay addrs which can be used for browser WebRTC signalling
+      webSockets: 0, // WebSockets circuit relay addrs which can be used for browser WebRTC signalling
+      webSocketsSecure: 0 // Secure WebSockets circuit relay addrs which can be used for browser WebRTC signalling
     }
 
     if (multiaddrs == null) {
@@ -30,23 +30,25 @@ export const NodeInfo: React.FC<NodeInfoProps> = () => {
     }
     return multiaddrs.reduce(
       (acc: typeof base, addr: Multiaddr) => {
+        acc.listeningAddrs++
         if (Circuit.exactMatch(addr)) {
           acc.circuitRelayAddrs++
-        } else if (WebRTC.exactMatch(addr)) {
-          acc.webRtc++
-        } else if (WebRTCDirect.exactMatch(addr)) {
-          acc.webRtcDirect++
-        } else if (WebTransport.exactMatch(addr)) {
-          acc.webTransport++
-        } else if (WebSockets.exactMatch(addr)) {
-          acc.webSockets++
-        } else if (WebSocketsSecure.exactMatch(addr)) {
-          acc.webSocketsSecure++
-        } else {
-          // eslint-disable-next-line no-console
-          console.log('unrecognized listen addr', addr.toString())
         }
-        acc.listeningAddrs++
+        if (WebRTC.exactMatch(addr)) {
+          acc.webRtc++
+        }
+        if (Circuit.exactMatch(addr) && WebRTCDirect.matches(addr)) {
+          acc.webRtcDirect++
+        }
+        if (Circuit.exactMatch(addr) && WebTransport.matches(addr)) {
+          acc.webTransport++
+        }
+        if (Circuit.exactMatch(addr) && WebSockets.matches(addr)) {
+          acc.webSockets++
+        }
+        if (Circuit.exactMatch(addr) && WebSocketsSecure.matches(addr)) {
+          acc.webSocketsSecure++
+        }
         return acc
       },
       base
@@ -83,8 +85,13 @@ export const NodeInfo: React.FC<NodeInfoProps> = () => {
   return (
     <div className='ml2 pb2 f5 gray-muted montserrat mw7'>
       <NodeInfoDetail label='Peer ID' value={peerId} />
-      <NodeInfoDetail label='ListeningAddrs' value={`${listeningAddrs} (relayed: ${circuitRelayAddrs}, webRtc: ${webRtc}, webRtcDirect: ${webRtcDirect}, webTransport: ${webTransport}, ws: ${webSockets}, wss: ${webSocketsSecure})`} />
-      <NodeInfoDetail label='Dialable over WebRTC' value={` ${webRtc > 0 ? '✅' : '❌'}`} />
+      <NodeInfoDetail label='ListeningAddrs' value={`${listeningAddrs} (relayed: ${circuitRelayAddrs}, WebRTC: ${webRtc}, Secure WebSockets: ${webSocketsSecure}, WebRTC Direct: ${webRtcDirect}, WebTransport: ${webTransport}, WebSockets: ${webSockets})`} />
+      {/*
+        Dialable from other Browsers exclude:
+        - WebTransport which is not included in the default Helia transports due to flaky browser support.
+        - Normal WebSockets which is blocked in secure contexts.
+      */}
+      <NodeInfoDetail label='Dialable from other Browsers' value={` ${(webRtcDirect + webSocketsSecure) > 0 ? '✅' : '❌'}`} />
       <NodeInfoDetail label='Connections' value={`${totalConns} (${inboundConns} in, ${outboundConns} out, ${unlimitedConns} unlimited)`} />
     </div>
   )
